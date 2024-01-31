70f393a9b6b0f659cab7c55eb8218a674df3cc38
alex lima <delimaferreiraalex@gmail.com>
2024-01-31 12:47:22 -0300

    initial commit

---
+++ src/main/java/com/luminahi/app/Employee.java
@@ -0,0 +1,70 @@
+package com.luminahi.app;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+    
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String role;
+    
+    public Employee() {}
+
+    public Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, role);
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        
+        Employee employee = (Employee) obj;
+        
+        return Objects.equals(this.id, employee.id) &&
+                Objects.equals(this.name, employee.name) &&
+                Objects.equals(this.role, employee.role);
+    }
+    
+    @Override
+    public String toString() {
+      return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';    
+    }
+}
